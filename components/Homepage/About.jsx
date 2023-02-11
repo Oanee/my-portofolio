660288528606020d26f65fc051392de27bb1aa3c
Oancea Alin <74875548+Oanee@users.noreply.github.com>
2023-02-11 23:28:41 +0200

    little changes

--- components/Homepage/About.jsx
+++ components/Homepage/About.jsx
@@ -10,10 +10,10 @@ export function About() {
           <p className="about-description-paragraph">
             Meet a passionate front-end developer with diverse experience in
             writing semantic, responsive HTML/CSS and working with different
-            frontend frameworks. Actively seeking for a job where I can use my
+            frontend frameworks. Actively seeking a job where I can use my
             knowledge to advance my career to the next stage. I also possess
             good written and oral communication skills, which enable me to
-            explain web app issues in easy to understand terms.{' '}
+            explain web app issues in easy to understand terms.
           </p>
         </div>
       </div>
