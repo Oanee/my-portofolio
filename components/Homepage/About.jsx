addcaed12ea73b16ebff165f360c3b193bb1c902
Oancea Alin <74875548+Oanee@users.noreply.github.com>
2023-02-12 15:41:55 +0200

    about photo

--- components/Homepage/About.jsx
+++ components/Homepage/About.jsx
@@ -6,15 +6,23 @@ export function About() {
           <h1 id="about-jump">About</h1>
         </header>
 
-        <div className="about-description">
-          <p className="about-description-paragraph">
-            Meet a passionate front-end developer with diverse experience in
-            writing semantic, responsive HTML/CSS and working with different
-            frontend frameworks. Actively seeking a job where I can use my
-            knowledge to advance my career to the next stage. I also possess
-            good written and oral communication skills, which enable me to
-            explain web app issues in easy to understand terms.
-          </p>
+        <div className="about-content">
+          <div className="about-content-photo">
+            <picture>
+              <img src="/images/Profile-Picture.png" alt="Me" />
+            </picture>
+          </div>
+
+          <div className="about-content-description">
+            <p className="about-content-description-paragraph">
+              Meet a passionate front-end developer with diverse experience in
+              writing semantic, responsive HTML/CSS and working with different
+              frontend frameworks. Actively seeking a job where I can use my
+              knowledge to advance my career to the next stage. I also possess
+              good written and oral communication skills, which enable me to
+              explain web app issues in easy to understand terms.
+            </p>
+          </div>
         </div>
       </div>
     </>
