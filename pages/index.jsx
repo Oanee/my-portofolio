341937ee1e8da73e2b79f73e6bb4cc09b2b583ca
Oancea Alin <74875548+Oanee@users.noreply.github.com>
2023-02-15 20:13:36 +0200

    ui changes

--- pages/index.jsx
+++ pages/index.jsx
@@ -4,6 +4,7 @@ import { About } from '@/components/Homepage/About';
 import Contact from '@/components/Homepage/Contact';
 import Skills from '@/components/Homepage/Skills';
 import Work from '@/components/Homepage/Work';
+import Link from 'next/link';
 
 export default function Home() {
   return (
@@ -12,14 +13,14 @@ export default function Home() {
         <Header></Header>
 
         <div className="header-scroll">
-          <div className="header-scroll-circle">
+          <Link href="#about-jump" className="header-scroll-circle">
             <div className="header-scroll-circle-wheel"></div>
             <div className="header-scroll-circle-wheel-arrows">
               <div className="arrow arrow-1"></div>
               <div className="arrow arrow-2"></div>
               <div className="arrow arrow-3"></div>
             </div>
-          </div>
+          </Link>
         </div>
       </header>
 
